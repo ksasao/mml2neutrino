36c5a1952af1ffa52a4ac6b8562a8c5e1fe06f05
b2ox <b2oxgm@gmail.com>
2020-03-06 21:39:18 +0900

    add: "-i mmlfile.txt" option
    mmlをテキストファイル入力できると便利かなと

--- src/MML2NEUTRINO/Program.cs
+++ src/MML2NEUTRINO/Program.cs
@@ -13,58 +13,64 @@ namespace MML2NEUTRINO
 
         static void Main(string[] args)
         {
-            NeutrinoController nc = new NeutrinoController();
-
-            string outputMusicXML = "output.musicxml";
-
             if (args.Length > 0)
             {
-                string mml = args[0];
-
-                IElement[] elements = null;
-                try
+                if (args[0] == "-i")
                 {
-                    elements = nc.Parse(mml);
+                    Translate(File.ReadAllText(args[1]);
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine(ex.Message);
-                    return;
+                    Translate(args[0]);
                 }
-
-                XElement xml = null;
-                try
-                {
-                    xml = nc.ConvertToMusicXML(elements);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return;
-                }
-
-                string musicXmlFile = "";
-                try
-                {
-                    musicXmlFile = Path.GetFullPath(outputMusicXML);
-                    xml.Save(musicXmlFile);
-                }
-                catch (IOException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return;
-                }
-
-                string outputWav = nc.RunNeutrino(musicXmlFile);
-
-                // Play wave file
-                Console.WriteLine($"Playing {outputWav}");
-                System.Media.SoundPlayer player = new System.Media.SoundPlayer(outputWav);
-                player.PlaySync();
             }
         }
 
+        static void Translate(string mml)
+        {
+            NeutrinoController nc = new NeutrinoController();
+            string outputMusicXML = "output.musicxml";
 
+            IElement[] elements = null;
+            try
+            {
+                elements = nc.Parse(mml);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            XElement xml = null;
+            try
+            {
+                xml = nc.ConvertToMusicXML(elements);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string musicXmlFile = "";
+            try
+            {
+                musicXmlFile = Path.GetFullPath(outputMusicXML);
+                xml.Save(musicXmlFile);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            string outputWav = nc.RunNeutrino(musicXmlFile);
+
+            // Play wave file
+            Console.WriteLine($"Playing {outputWav}");
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(outputWav);
+            player.PlaySync();
+        }
     }
-
 }
